4e1280d233f56fa653441daf67a2e3ce3330eed8
Zhiqing Qiu <zhqqi@microsoft.com>
2017-04-28 13:17:27 +0800

    add device api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var job = require('./routes/job');
+var device = require('./routes/device');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/job', job);
+app.use('/device', device);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/device.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var uuid = require('uuid');
+var device = express();
+router.get('/get_total', (req, res) => {
+    var connectionString = req.query.connection_string;
+    if (!connectionString) {
+        res.sendStatus(400);
+    }
+    var Registry = require('azure-iothub').Registry.fromConnectionString(connectionString);
+    Registry.list((err, deviceList) => {
+        if (err) {
+            res.status(500).send('Could not trigger job: ' + err.message);
+        } else {
+            var connectedNum = 0;
+            deviceList.forEach((device) => {
+                if (device.connectionState === "Connected") connectedNum++;
+            });
+            res.send({
+                registered: deviceList.length,
+                connected: connectedNum
+            });
+        }
+    });
+});
+
+module.exports = router
